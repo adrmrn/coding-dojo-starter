412c63b8f233ec296f8ec7dc9d7592a7c2a4e80d
Adrian M <contact@adrmrn.dev>
2022-10-01 18:16:49 +0200

    Define directories structure

---
+++ tests/KataTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class KataTest extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
